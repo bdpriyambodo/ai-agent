0b9692fd5e487f3230d49fc6d90773723437e565
bdpriyambodo <b.danang.priyambodo@gmail.com>
2025-11-26 16:48:48 +0700

    add calculator and get_files_info.py

---
+++ functions/get_files_info.py
@@ -0,0 +1,38 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    joined_path = os.path.join(working_directory, directory)
+    # print(joined_path)
+
+    # Normalize both paths (resolve "..", ".", etc.)
+    full_path = os.path.abspath(joined_path)
+    full_working_directory = os.path.abspath(working_directory)
+
+    if not full_path.startswith(full_working_directory):
+        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        return
+    if not os.path.isdir(full_path):
+        print(f'Error: "{directory}" is not a directory')
+        return
+    
+    # print('full path: ' + full_path)
+    # print('full working directory: ' + full_working_directory)
+    dir_list = os.listdir(full_path)
+    dir_fullpath_list = [(os.path.join(full_path,x)) for x in dir_list]
+    size_list = [os.path.getsize(x) for x in dir_fullpath_list]
+    checkdir_list = [os.path.isdir(x) for x in dir_fullpath_list]
+    full_list = list(zip(dir_list,size_list,checkdir_list))
+    string_list = [f"- {x[0]}: file_size={x[1]} bytes, is_dir={x[2]}" for x in full_list]
+    combined_string = "\n".join(string_list)
+    
+    # print(dir_list)
+    # print(dir_fullpath_list)
+    # print(size_list)
+    # print(file_list)
+    # print(full_list)
+    # print(string_list)
+    print(combined_string)
+
+
+
+# get_files_info(".", "calculator")    
